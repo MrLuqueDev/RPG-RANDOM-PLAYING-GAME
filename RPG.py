8fa0162b4a12a6a36b65829f31acb71c5840e9dd
Mr.Luque <hugoluque2008@gmail.com>
2024-10-22 10:21:06 +0200

    V2 RPG.py
    Updated some stuff and made some new functions. still not functional yet but we're gettings close

--- RPG.py
+++ RPG.py
@@ -1,8 +1,9 @@
-# TO DO: RANDOMIZE STATS
-# GET RANDOM ATTACK DAMAGE AND SHIT
-# RANDOM GENERATION (get random structures and stuff)
-# Random enemies (types and stats and names aswell) 
-# game objects
+# TO DO: RANDOMIZE STATS | DONE
+# GET RANDOM ATTACK DAMAGE AND SHIT | DONE
+# RANDOM GENERATION (get random structures and stuff) | done as well man wtf we're cooking brah
+# Random enemies (types and stats and names aswell) | sorta done????
+# game objects | lmao fuck no
+# if you're defending the enemy can't defend (and viceversa?) | WE HAVEN'T EVEN STARTED LLOLOLOLOLOOOLOL
 
 # --------------------------------------------------------------------------------
 
@@ -15,20 +16,40 @@ import math
 
 # import pyautogui
 
-# General variables
+# Game variables
 
-name = ""
+game = True
+turn = None
+battling = None
+encounter = None
+defending = None
+edefending = None
+# structure variables
+
 structure_name = ""
 has_chest = None
 has_enemies = None
-keys = None
-coins = None
-bombs = None
+
+# player variables
+
+name = ""
+keys = 0
+coins = 0
+bombs = 0
+level = 0
 HP = None
 RST = None
 ATK = None
 SPD = None
 LCK = None
+
+# enemy variables
+
+eHP = None
+eRST = None
+eATK = None
+eSPD = None
+elevel = 0
 
 # FUNCTIONS :3
 
@@ -45,6 +66,18 @@ def defstats():
     SPD = 10
     LCK = random.randint(-10, 10)
 
+def defenemystats():
+    global eHP
+    global eRST
+    global eATK
+    global eSPD
+    global elevel
+
+    eHP = random.randint(50, 90) + level
+    eRST = random.randint(1,5)
+    eATK = round(math.log(random.randint(4,10)))
+    eSPD = 2
+
 def RandName(char_min, char_max):
     for i in range(random.randint(char_min, char_max)):
             global name
@@ -53,20 +86,50 @@ def RandName(char_min, char_max):
 def wait(s):
      time.sleep(s)
 
+def GetStructure():
+    global structure_name
+    global has_chest
+    global has_enemies
+    has_enemies = random.randint(0,1)
+    encounter = random.randint(0,1)
+    has_chest = random.randint(0,1)
+    randomstructure = random.randint(1,7)
+    if randomstructure == 1:
+        structure_name = "Village"
+    elif randomstructure == 2:
+        structure_name = "Forest"
+    elif randomstructure == 3:
+        structure_name = "Temple"
+    elif randomstructure == 4:
+        structure_name = "Plains"
+    elif randomstructure == 5:
+        structure_name = "Desert"
+    elif randomstructure == 6:
+        structure_name = "Swamp"
+    else:
+        structure_name = "Dungeon"
+
 def Structure(s_name, s_chest, s_enemies):
 
     # when called, the code must specify the name, if it has chests and if it has enemies.
     # here we make it check so if the name is X then it will do this and that
 
+    strname = str(s_name)
+
     if s_name == "Village":
         print("You are in a village")
         if s_chest == 1:
-            print("This Village has a chest!")
-
+            print("This", strname, "has a chest!")
+        else:
+            print("This", strname, "doesn't have a chest.")
     elif s_name == "Forest":
         print("You are in a forest")
     elif s_name == "Temple":
         print("You are in a temple")
+        if s_chest == 1:
+            print("This", strname, "has a chest!")
+        else:
+            print("This", strname, "doesn't have a chest.")
     elif s_name == "Plains":
         print("You are in a plains")
     elif s_name == "Desert":
@@ -75,37 +138,91 @@ def Structure(s_name, s_chest, s_enemies):
         print("You are in a swamp")
     else:
         print("You are in a dungeon ")
+        if s_chest == 1:
+            print("This", strname, "has a chest!")
+        else:
+            print("This", strname, "doesn't have a chest.")
+    if has_enemies == 1 and encounter == 1:
+        battle()
+# here we go
 
-def GetStructureName():
-    # firstly get the name
-    global structure_name
-    global has_chest
-    global has_enemies
-    has_enemies = random.randint(0,1)
-    has_chest = random.randint(0,1)
-    randomstructure = random.randint(1,7)
-    if randomstructure == 1:
-        return "Village"
-    elif randomstructure == 2:
-        return "Forest"
-    elif randomstructure == 3:
-        return "Temple"
-    elif randomstructure == 4:
-        return "Plains"
-    elif randomstructure == 5:
-        return "Desert"
-    elif randomstructure == 6:
-        return "Swamp"
-    else:
-        return "Dungeon"
+def attack():
 
-def getstructurechests():
-        # now set if it should have a chest or not
+    # turn 0 is the enemy's turn, 1 is your turn
 
-    if structure_name == "Village":
-        return
+    global turn
+
+    if turn == 0:
+        HP -= eATK + (RST*0.5)
+    elif turn == 1:
+        eHP -= ATK + (eRST*0.5)
+        
+
+def defend():
+    global turn
+
+    if turn == 1:
+        RST * 2
+    elif turn == 0:
+        eRST * 2
+
+def StopDefend():
+    global turn
+    if turn == 1:
+        RST // 2
+    elif turn == 0:
+        eRST // 2
+
+def item():
+    # we need a list with all items
+    return
+
+def BattleEnd():
+    battling == False
+
+def battle():
+
+    global battling
+
+    while battling == True:
+        rnd_action = random.randint(1,3)
+
+        if HP <= 0:
+           print(name, "has died\nlife stats")
+           game = False
+        if eHP <= 0:
+            # kills the enemy and gives you (xp) money, bombs & keys
+            return
+
+        if eHP <= 10:
+            eflee = random.randint(0,1)
+
+        if eflee == 1:
+            print("the enemy is fleeing...")
+
+        if HP <= 10:
+            flee = random.randint(0,1)
+
+        if flee == 1:
+            print(name, "decided to flee")
+
+
+        if rnd_action == 1:
+            print(name, "has decided to attack!")
+            attack(1)
+        elif rnd_action == 2:
+            print(name, "has decided to defend!")
+            defend()
+        elif rnd_action == 3:
+            print(name, "has decided to use an item!")
+            item()
+        if battling == False:
+            BattleEnd()
 
 # Main game code
+# hardcode a game loop so whenever the player dies it can break, and also the structures and shit can loop indefinetely
+
+# while run == true:
 
 question = int(input("Want a random name (0) or your own name (1)?: "))
 
@@ -134,7 +251,8 @@ print("LUCK (LCK):", LCK)
 
 # game starts
 
-
-GetStructureName()
-# make a function called getstructure that straight up does the 3 get structure functions
-Structure("Village", 1, 0)
+while game == True:
+    if HP <= 0:
+        break
+    GetStructureName()
+    Structure(structure_name, has_chest, has_enemies)
