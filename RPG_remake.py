7587d2b5769f79765e9be963a4c97fa14917638a
Mr.Luque <hugoluque2008@gmail.com>
2025-01-22 13:05:37 +0100

    albion online es un mmorpg no lineal en el que escribes tu propia historia sin limitarte a seguir un camino prefijado. explora un amplio mundo de posibilidades con el nuevo sistema de economia de albion.
    god, end my suffering

--- RPG_remake.py
+++ RPG_remake.py
@@ -5,6 +5,10 @@ import time
 # RPG.py remake because our code was an UNBEARABLE mess.
 # TODO: fucking kys
 
+# Game Variables
+
+name = ""
+
 # stats
 
 HP = None
@@ -34,10 +38,51 @@ structure = None
 chest = None # bool
 enemies = None # bool
 
-# stats functions
+# Game functions
 
 def defstats():
-    
+    return "Stats"
+
+def FunNames():
+
+    global name, game, HP, RST, LCK, ATK, SPD
+
+    if name == "hugo":
+        wait(1)
+        print("hugo used: Suicide!")
+        wait(0.5)
+        print("It's super effective!")
+        wait(0.5)
+        print("hugo is now fucking dead!")
+        game = False
+    elif name == "frisk":
+        input(
+            "Warning, This name will make your life a literal living nightmare. do you wish to procceed? (y/n): "
+        )
+        wait(1.5)
+        print("well too bad, you already chose it now HAAHAHAHAHAHAHA")
+        HP = 50
+        RST = 1
+        ATK = 5
+        SPD = 5
+        LCK = -10
+    elif name == "govus":
+        HP = 149
+        RST = 1
+        ATK = 2
+        SPD = 10
+        LCK = 10
+    elif name == "jester":
+        HP = 50
+        RST = 1
+        ATK = 5
+        SPD = 5
+        LCK = 10
+
+def RandName(char_min, char_max):
+    for i in range(random.randint(char_min, char_max)):
+        global name
+        name += random.choice("abcdefghijklmnopqrstuvwxyz")
 
 # Dev functions
 
@@ -82,6 +127,22 @@ wait(0.5)
 print("             [RPG]            ")
 wait(0.5)
 print("------------------------------")
+
+question = int(input("Want a random name (0) or your own name (1)?: "))
+
+if question == 0:
+    RandName(1, 10)
+elif question == 1:
+    name = input("What is your name?: ")
+else:
+    print("That's not a valid answer, picking a random name instead")
+    RandName(1, 10)
+wait(1)
+print("your name is: ", name)
+wait(0.5)
+defstats()
+FunNames()
+wait(2.5)
 
 while True:
     genstructure(getstructure())
